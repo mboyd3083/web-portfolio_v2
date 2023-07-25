e265657695d2532ccf0b18e5ed42b8c31b401d59
Mike Boyd <mboyd3083@gmail.com>
2023-07-24 22:17:45 -0400

    modified file

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import "./Home.css";
 import Social from "./Social";
 import Data from "./HomeData";
+// import profileGraphic from "../../assets/profile-graphic.png";
 const Home = () => {
   return (
     <section className="home section" id="home">
       <div className="home__container container grid">
         <div className="home__content grid">
           <Social />
-          {/* <div className="home__img"></div> */}
+          {/* <div className="home__img">
+           <img src={profileGraphic} alt="" />
+          </div> */}
           <Data />
         </div>
       </div>
